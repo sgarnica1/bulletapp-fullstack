009f41e088fbb2d77faee4a9d7885ca6765fafb6
Sergio Garnica <sgarnica1902@gmail.com>
2021-08-29 17:41:55 -0500

    Dropdown sidevar navigation added

---
+++ assets/js/dropdown-navbar.js
@@ -0,0 +1,24 @@
+const dropdownContainer = document.querySelectorAll('.navlist__dropdown')
+
+const showDropdown = () => {
+  for (container of dropdownContainer) {
+    let button = container.querySelector('button')
+    
+    button.addEventListener('click', (e) => {
+      let currentDropdown = e.srcElement.closest('li')
+      let button = currentDropdown.querySelector('button')
+      let ul = currentDropdown.querySelector('ul')
+      // console.log(button)
+
+      if (!button.classList.contains('show')) {
+        button.classList.add('show')
+        ul.classList.add('show')
+      } else {
+        button.classList.remove('show')
+        ul.classList.remove('show')
+      }
+    })
+  }
+}
+
+showDropdown()
